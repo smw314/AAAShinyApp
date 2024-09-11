9f633d56b7bb0b86198d26aaa7321fb94cdf0557
Sam Wirth <92800725+smw314@users.noreply.github.com>
2024-09-10 22:38:51 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,12 +1,4 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+# Load required libraries
 library(shiny)
 library(tidyverse)
 library(DT)
@@ -18,86 +10,123 @@ library(shinyWidgets)
 library(shinythemes)
 library(shinycssloaders)
 
-pitcher_data <- read_csv('pitcher_data.csv')
-teams <- read_csv('team.csv')
-batting_teams <- read_csv('batting_team.csv')
-batter_names <- read_csv('batter_names.csv')
-pitcher_names <- read_csv('pitcher_names.csv')
+# Load datasets
+pitcher_data <- read_csv('pitcher_data.csv')   # Data for pitchers
+teams <- read_csv('team.csv')                  # List of teams
+batting_teams <- read_csv('batting_team.csv')  # List of batting teams
+batter_names <- read_csv('batter_names.csv')   # List of batter names
+pitcher_names <- read_csv('pitcher_names.csv') # List of pitcher names
 
+# Define UI layout for the Shiny app
 shinyUI(fluidPage(
-  theme = shinytheme("cerulean"),
+  theme = shinytheme("cerulean"),  # Set theme for the UI
   
+  # Main title and author information
   titlePanel("2023 Triple-A and FSL Data"),
   h4("By Sam Wirth @SamWirthSports"),
   h5("Last Updated: 10/01/2023"),
-  navlistPanel("Pitchers",
+  
+  # Navigation panel for different sections of the app
+  navlistPanel("Pitchers",  # Tab for pitchers
+  
+    # Tab for viewing data of all pitchers
     tabPanel("All", 
-             pickerInput(
+             pickerInput(  # Dropdown for selecting league
                inputId = "league_pitcher",
                label = "Select League",
-               choices = c("FSL", "PCL", "INT"),
-               multiple = TRUE,
-               selected = c("FSL", "PCL", "INT")
-             ), dataTableOutput("table")),
+               choices = c("FSL", "PCL", "INT"),  # League options
+               multiple = TRUE,  # Allow multiple selections
+               selected = c("FSL", "PCL", "INT")  # Default selection
+             ),
+             dataTableOutput("table")  # Output data table for pitcher data
+    ),
+    
+    # Tab for viewing data by team
     tabPanel("Team", 
-             pickerInput(
+             pickerInput(  # Dropdown for selecting team
                inputId = "team",
                label = "Enter Team", 
-               choices = c(teams),
+               choices = c(teams),  # Choices from loaded team data
                selected = NULL,
                options = list(
-                 `live-search` = TRUE)),
-             # selectInput("team", "Enter Team", choices = c(unique(ml_pbp$team))),
-             dataTableOutput("teamtable")),
+                 `live-search` = TRUE  # Enable live search for teams
+               )
+             ),
+             dataTableOutput("teamtable")  # Output table for team-specific data
+    ),
+    
+    # Tab for viewing individual pitcher data
     tabPanel("Individual", 
-             pickerInput(
+             pickerInput(  # Dropdown for selecting pitcher by name
                inputId = "filter",
                label = "Enter Pitcher Name", 
-               choices = c(unique(pitcher_data$filter)),
+               choices = c(unique(pitcher_data$filter)),  # Unique names from pitcher data
                selected = NULL,
                options = list(
-                 `live-search` = TRUE)),
-             # selectInput("filter", "Enter Pitcher Name", choices = c(unique(ml_pbp$matchup.pitcher.fullName))),
-             fluidRow(column(12, dataTableOutput("table2")),
-                      column(4, plotOutput("plot")),
-                      column(4, plotOutput("plot3")),
-                      column(4, plotOutput("plot4")),
-                      column(4, plotOutput("plot5")),
-                      column(4, plotOutput("plot6")),
-                      column(4, plotOutput("plot7")))),
-    'Batters',
+                 `live-search` = TRUE  # Enable live search for pitcher names
+               )
+             ),
+             
+             # Layout for displaying pitcher data table and multiple plots
+             fluidRow(
+               column(12, dataTableOutput("table2")),  # Main data table for individual pitchers
+               column(4, plotOutput("plot")),  # Pitcher-related plot outputs
+               column(4, plotOutput("plot3")),
+               column(4, plotOutput("plot4")),
+               column(4, plotOutput("plot5")),
+               column(4, plotOutput("plot6")),
+               column(4, plotOutput("plot7"))
+             )
+    ),
+    
+    'Batters',  # Tab for batters section
+    
+    # Tab for viewing data of all batters
     tabPanel("All", 
-             pickerInput(
+             pickerInput(  # Dropdown for selecting league for batters
                inputId = "league_batter",
                label = "Select League",
-               choices = c("FSL", "PCL", "INT"),
-               multiple = TRUE,
-               selected = c("FSL", "PCL", "INT") # Default to all leagues
+               choices = c("FSL", "PCL", "INT"),  # League options
+               multiple = TRUE,  # Allow multiple selections
+               selected = c("FSL", "PCL", "INT")  # Default selection
              ),
-             dataTableOutput("table3")),
-    tabPanel("Team",
-             pickerInput(
+             dataTableOutput("table3")  # Output data table for batter data
+    ),
+    
+    # Tab for viewing batter data by team
+    tabPanel("Team", 
+             pickerInput(  # Dropdown for selecting team for batters
                inputId = "team2",
                label = "Enter Team", 
-               choices = c(batting_teams),
+               choices = c(batting_teams),  # Choices from loaded batting teams
                selected = NULL,
                options = list(
-                 `live-search` = TRUE)),
-             # selectInput("team2", "Enter Team", choices = c(unique(ml_pbp$batting_team))),
-             dataTableOutput("teamtable2")),
-    tabPanel("Individual",
-             pickerInput(
+                 `live-search` = TRUE  # Enable live search for batting teams
+               )
+             ),
+             dataTableOutput("teamtable2")  # Output table for team-specific batter data
+    ),
+    
+    # Tab for viewing individual batter data
+    tabPanel("Individual", 
+             pickerInput(  # Dropdown for selecting batter by name
                inputId = "filter2",
                label = "Enter Batter Name", 
-               choices = c(batter_names),
+               choices = c(batter_names),  # Unique names from batter data
                selected = NULL,
                options = list(
-                 `live-search` = TRUE)),
-             # selectInput("filter2", "Enter Batter Name", choices = c(unique(ml_pbp$matchup.batter.fullName))),
-             dataTableOutput("table4"),
-             fluidRow(column(6, plotOutput("plot2")),
-                      column(6, plotOutput("plot99")))),
-    widths = c(1, 11)
+                 `live-search` = TRUE  # Enable live search for batter names
+               )
+             ),
+             dataTableOutput("table4"),  # Output table for individual batter data
+             
+             # Layout for displaying batter plots
+             fluidRow(
+               column(6, plotOutput("plot2")),  # Main plot for individual batters
+               column(6, plotOutput("plot99"))  # Additional plot for batter data
+             )
+    ),
+    
+    widths = c(1, 11)  # Adjust widths of the navigation list and content
   )
-)
-)
+))
